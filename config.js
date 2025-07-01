6047e900ac37324deea4087fe0601a8f770e6603
tedtomo <tomoyoshi.matsuda0809@gmail.com>
2025-07-01 18:33:52 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 実際の値を入力してください
 const CONFIG = {
     // Google Sheets API キー
-    API_KEY: 'ここにAPIキーを入力',
+    API_KEY: 'AIzaSyDFdq2UyUXdLNitYUdgpdUlfqB5TL7oUQg',
     
     // Google Apps Script URL（完了ボタン用）
     SCRIPT_URL: 'ここにGoogle Apps Script URLを入力',
